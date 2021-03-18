a7d50fe2576af23cebccafd345d9586795e95fd0
Eric T <9063787+tsairinius@users.noreply.github.com>
2021-03-18 16:09:58 -0700

    Added arrow to HoverMessage component

--- src/components/HoverMessage.js
+++ src/components/HoverMessage.js
@@ -28,7 +28,6 @@ const StyledHoverMessage = styled.div`
     padding: 0.3em;
     font-size: 0.8rem;
     border-radius: 3px;
-    border: 1px solid black;
     white-space: nowrap;
     z-index: 1;
 
@@ -39,6 +38,16 @@ const StyledHoverMessage = styled.div`
         &:first-child, &:nth-child(2) {
             font-weight: bold;
         }
+    }
+
+    &::after {
+        content: "";
+        position: absolute;
+        top: 100%;
+        left: 10%;
+        border-color: white transparent transparent transparent;
+        border-style: solid;
+        border-width: 5px;
     }
 `;
 
